1bf68ddf56b27b538a46f0953f00391fa4cf43ad
Aisha <aishakozak@hotmail.com>
2024-06-15 13:12:44 +1000

    proximity threshold testing

--- astride/detect3.py
+++ astride/detect3.py
@@ -73,7 +73,7 @@ class Streak:
     """
     def __init__(self, filename, remove_bkg='map', bkg_box_size=50,
                  contour_threshold=3., min_points=10, shape_cut=0.2,
-                 area_cut=20., radius_dev_cut=0.5, connectivity_angle=30.,proximity_threshold=1.,
+                 area_cut=20., radius_dev_cut=0.5, connectivity_angle=30.,proximity_threshold=1000.,
                  fully_connected='high', output_path=None):
         hdulist = fits.open(filename)
         raw_image = hdulist[0].data.astype(np.float64)
