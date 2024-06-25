421aa750fa1fcba285a500aba25ad36d21c68c2d
Aisha <aishakozak@hotmail.com>
2024-06-25 12:12:21 +1000

    prox threshold w angle range

--- astride/utils/edge4.py
+++ astride/utils/edge4.py
@@ -1,23 +1,53 @@
 import numpy as np
 from scipy.optimize import leastsq
+from math import atan2, degrees
 
 class EDGE:
+    """
+    Detect edges (i.e. borders) using the input contours.
+
+    Parameters
+    ----------
+    contours : array_like
+        An array containing contour list.
+    min_points : int, optional
+        The number of minimum data points in each edge.
+    shape_cut : float, optional
+        An empirical shape factor cut.
+    area_cut : float, optional
+        An empirical area cut.
+    radius_dev_cut : float, optional
+        An empirical radius deviation cut.
+    connectivity_angle: float, optional
+        A maximum angle to connect each separated edge.
+    """
     def __init__(self, contours, min_points=0, shape_cut=0.2,
                  area_cut=10., radius_dev_cut=0.5, connectivity_angle=45.):
+        # Set global values.
         self.shape_cut = shape_cut
         self.area_cut = area_cut
         self.radius_dev_cut = radius_dev_cut
         self.connectivity_angle = connectivity_angle
 
+        # Set structure.
         self.edges = []
         for i in range(len(contours)):
+            # Remove unclosed contours.
             if contours[i][0][0] != contours[i][-1][0] or \
                contours[i][0][1] != contours[i][-1][1] or \
                len(contours[i]) <= min_points:
                 continue
 
+            # All variables are self-explaining except the radius_deviation
+            # and the connectivity.
+            # The radius_deviation is the ratio of the standard deviation
+            # of the distances from the center to the radius.
+            # The connectivity indicates an index of an edge likely to be
+            # connected with the current edge. -1 indicates no connectivity.
             self.edges.append({
                 'index': i + 1,
+                # Note that the contours returned from the scikit-image
+                # is a list of [row, columns]
                 'x': contours[i][::, 1],
                 'y': contours[i][::, 0],
                 'x_center': 0., 'y_center': 0.,
@@ -26,28 +56,37 @@ class EDGE:
                 'radius_deviation': 0.,
                 'slope': 0., 'intercept': 0.,
                 'connectivity': -1,
+                # For plotting a box surrounding the edge.
                 'x_min': 0., 'x_max': 0.,
                 'y_min': 0., 'y_max': 0.,
+                # For plotting. To check if this edge
+                # is surrounded by a box already.
                 'box_plotted': False,
-                'slope_angle': 0.,
-                'cov_matrix': None,  # Add for tensor of light
-                'principal_dir': None  # Add for principal direction
+                'slope_angle': 0.  # Initialize slope_angle
             })
 
     def quantify(self):
+        """Quantify shape of the contours."""
         four_pi = 4. * np.pi
         for edge in self.edges:
+            # Positions
             x = edge['x']
             y = edge['y']
 
-            A, perimeter, x_center, y_center, distances = self.get_shape_factor(x, y)
+            A, perimeter, x_center, y_center, distances = \
+                self.get_shape_factor(x, y)
 
+            # Set values.
             edge['area'] = A
             edge['perimeter'] = perimeter
             edge['x_center'] = x_center
             edge['y_center'] = y_center
-            edge['shape_factor'] = four_pi * edge['area'] / edge['perimeter'] ** 2.
+            # Circle is 1. Rectangle is 0.78. Thread-like is close to zero.
+            edge['shape_factor'] = four_pi * edge['area'] / \
+                                   edge['perimeter'] ** 2.
 
+            # We assume that the radius of the edge
+            # is the median value of the distances from the center.
             radius = np.median(distances)
             edge['radius_deviation'] = np.std(distances - radius) / radius
 
@@ -55,64 +94,134 @@ class EDGE:
             edge['x_max'] = np.max(x)
             edge['y_min'] = np.min(y)
             edge['y_max'] = np.max(y)
-            
-            # Compute covariance matrix and principal direction
-            edge['cov_matrix'], edge['principal_dir'] = self.compute_cov_matrix_and_principal_dir(x, y)
 
     def get_shape_factor(self, x, y):
+        """
+        Return values related to the shape based on x and y.
+
+        Parameters
+        ----------
+        x : array_like
+            An array of x coordinates.
+        y : array_like
+            An array of y coordinates.
+
+        Returns
+        -------
+        area : float
+            Area of the border.
+        perimeter : float
+            Perimeter of the border.
+        x_center : float
+            X center coordinate.
+        y_center : float
+            Y center coordinate.
+        distances : numpy.ndarray
+            Distances from the center to each border element.
+        """
+
+        # Area.
         xyxy = (x[:-1] * y[1:] - x[1:] * y[:-1])
         A = 1. / 2. * np.sum(xyxy)
 
+        # X and Y center.
         one_sixth_a = 1. / (6. * A)
         x_center = one_sixth_a * np.sum((x[:-1] + x[1:]) * xyxy)
         y_center = one_sixth_a * np.sum((y[:-1] + y[1:]) * xyxy)
 
-        perimeter = np.sum(np.sqrt((x[1:] - x[:-1])**2 + (y[1:] - y[:-1])**2))
+        # Perimeter.
+        perimeter = np.sum(np.sqrt((x[1:] - x[:-1])**2 +
+                                   (y[1:] - y[:-1])**2))
 
+        # Distances from the center.
         distances = np.sqrt((x - x_center)**2 + (y - y_center)**2)
 
         return np.abs(A), perimeter, x_center, y_center, distances
-
-    def compute_cov_matrix_and_principal_dir(self, x, y):
-        # Center the points
-        x_centered = x - np.mean(x)
-        y_centered = y - np.mean(y)
-        
-        # Create the covariance matrix
-        cov_matrix = np.cov(x_centered, y_centered)
-        
-        # Compute eigenvalues and eigenvectors
-        eigvals, eigvecs = np.linalg.eig(cov_matrix)
-        
-        # The principal direction is the eigenvector with the largest eigenvalue
-        principal_dir = eigvecs[:, np.argmax(eigvals)]
-        
-        return cov_matrix, principal_dir
 
     def get_edges(self):
         return self.edges
 
     def filter_edges(self):
+        """Remove edges unlikely to be streaks."""
         filtered_edges = []
         filtered_cnt = 1
         for edge in self.edges:
             if edge['shape_factor'] <= self.shape_cut and \
                edge['area'] >= self.area_cut and \
                edge['radius_deviation'] >= self.radius_dev_cut:
+                # Reset index, incremental from 1.
                 edge['index'] = filtered_cnt
                 filtered_edges.append(edge)
                 filtered_cnt += 1
 
+        # Set filtered edges.
         self.edges = filtered_edges
 
     def residuals(self, theta, x, y):
+        """
+        Residual for a straight line.
+
+        Parameters
+        ----------
+        theta : list of float
+            Coefficients of float[2].
+        x : array_like
+            An array of x values.
+        y : array_like
+            An array of y values.
+
+        Returns
+        -------
+        residual : float
+            Residuals.
+        """
+
         residu = y - (theta[0] * x + theta[1])
+
         return residu
     
     def check_center_proximity(self, edge1, edge2, proximity_threshold):
+        """
+        Check if the centers of two edges are within the proximity threshold.
+
+        Parameters
+        ----------
+        edge1 : dict
+            Dictionary representing the first edge.
+        edge2 : dict
+            Dictionary representing the second edge.
+        proximity_threshold : float
+            The maximum allowable distance between the centers of two edges.
+
+        Returns
+        -------
+        bool
+            True if the centers are within the proximity threshold, False otherwise.
+        """
         distance = np.sqrt((edge1['x_center'] - edge2['x_center'])**2 +
                            (edge1['y_center'] - edge2['y_center'])**2)
         return distance <= proximity_threshold
+
+    def angle_between(self, edge1, edge2):
+        """
+        Calculate the angle in degrees between two edges' center points.
+
+        Parameters
+        ----------
+        edge1 : dict
+            Dictionary representing the first edge.
+        edge2 : dict
+            Dictionary representing the second edge.
+
+        Returns
+        -------
+        float
+            Angle in degrees between the center points of two edges.
+        """
+        v1 = [edge1['x_center'], edge1['y_center']]
+        v2 = [edge2['x_center'], edge2['y_center']]
+        angle = degrees(atan2(v2[1] - v1[1], v2[0] - v1[0]))
+        return angle
 
     def merge_edges(self, edge1, edge2):
         merged_edge = {
@@ -128,108 +237,39 @@ class EDGE:
             'x_min': 0., 'x_max': 0.,
             'y_min': 0., 'y_max': 0.,
             'box_plotted': False,
-            'slope_angle': 0.,
-            'cov_matrix': None,
-            'principal_dir': None
+            'slope_angle': 0.  # Initialize slope_angle
         }
         return merged_edge
 
-    def connect_edges(self, proximity_threshold=500, angle_range=10):
+    def connect_edges(self, proximity_threshold=5000, angle_range=(0, 45)):
+        """Connect detected edges based on their slopes and proximity threshold within an angle range."""
+        # Fitting a straight line to each edge.
         p0 = [0., 0.]
-        radian2angle = 180. / np.pi
         for edge in self.edges:
-            p1, s = leastsq(self.residuals, p0, args=(edge['x'][:-1], edge['y'][:-1]))
-            edge['slope'] = p1[0]
-            edge['intercept'] = p1[1]
-            edge['slope_angle'] = np.arctan(edge['slope']) * radian2angle
+            x = edge['x']
+            y = edge['y']
+            result = leastsq(self.residuals, p0, args=(x, y))
+            edge['slope'] = result[0][0]
+            edge['intercept'] = result[0][1]
+            edge['slope_angle'] = degrees(atan2(edge['slope'], 1))  # Calculate slope angle
 
-        len_edges = len(self.edges)
-        i = 0
-        while i < len_edges - 1:
-            j = i + 1
-            while j < len_edges:
-                angle_difference = np.abs(self.edges[i]['slope_angle'] - self.edges[j]['slope_angle'])
-                if angle_difference <= self.connectivity_angle:
-                    if angle_difference <= angle_range and self.check_center_proximity(self.edges[i], self.edges[j], proximity_threshold):
-                        principal_dot = np.dot(self.edges[i]['principal_dir'], self.edges[j]['principal_dir'])
-                        if np.abs(principal_dot) > 0.9:  # Check if directions are almost parallel
-                            self.edges[i] = self.merge_edges(self.edges[i], self.edges[j])
-                            del self.edges[j]
-                            len_edges -= 1
-                        else:
-                            j += 1
-                    else:
-                        j += 1
-                else:
-                    j += 1
-            i += 1
+        # Loop to find and connect edges within angle range and proximity threshold.
+        edge_connected = []
+        for i in range(len(self.edges)):
+            if i in edge_connected:
+                continue
+            for j in range(i + 1, len(self.edges)):
+                if j in edge_connected:
+                    continue
+                edge_i = self.edges[i]
+                edge_j = self.edges[j]
+                angle_ij = abs(self.angle_between(edge_i, edge_j))
+                # Check if the angle is within the specified range and the centers are within the proximity threshold.
+                if self.check_center_proximity(edge_i, edge_j, proximity_threshold) and angle_range[0] <= angle_ij <= angle_range[1]:
+                    merged_edge = self.merge_edges(edge_i, edge_j)
+                    self.edges[i] = merged_edge
+                    edge_connected.append(j)
 
-if __name__ == '__main__':
-    import pylab as pl
+        # Remove merged edges from the list.
+        self.edges = [edge for k, edge in enumerate(self.edges) if k not in edge_connected]
 
-    contours = np.array([[[985.2156529,  385.],
-                         [985.21551898,  384.],
-                         [985.,  383.78486061],
-                         [984.,  383.7864848],
-                         [983.21207242,  383.],
-                         [983.21186237,  382.],
-                         [983.,  381.83091953],
-                         [982.,  381.16390331],
-                         [981.93404467,  381.],
-                         [982.,  380.95885879],
-                         [982.96034405,  380.],
-                         [982.98009999,  379.],
-                         [983.,  378.98009999],
-                         [983.,  378.],
-                         [982.,  377.],
-                         [981.,  377.],
-                         [980.,  376.27694305],
-                         [979.27696018,  376.],
-                         [979.,  375.69899034],
-                         [978.85730809,  375.],
-                         [978.66888703,  374.],
-                         [978.60533867,  373.],
-                         [978.57569543,  372.82404834],
-                         [978.57569543,  373.],
-                         [978.22733739,  373.],
-                         [978.,  373.22737824],
-                         [977.26820563,  374.],
-                         [977.,  374.3434309],
-                         [976.28618803,  375.],
-                         [976.,  375.59227883],
-                         [975.91352882,  376.],
-                         [975.9130127,  377.],
-                         [975.,  377.91354762],
-                         [974.91345822,  378.],
-                         [974.91318255,  379.],
-                         [975.,  379.15387685],
-                         [976.,  380.16790882],
-                         [976.08699401,  380.],
-                         [977.,  379.08221136],
-                         [977.12953692,  379.],
-                         [978.,  378.4244201],
-                         [978.46654653,  378.],
-                         [979.,  377.55453658],
-                         [980.,  377.57358196],
-                         [980.4267619,  378.],
-                         [980.5732381,  378.],
-                         [981.,  378.42680567],
-                         [981.3685691,  378.],
-                         [981.6714309,  378.],
-                         [982.,  378.32700688],
-                         [983.,  378.12718606],
-                         [983.66132027,  379.],
-                         [984.15637254,  379.],
-                         [985.,  379.76523402],
-                         [985.00000548,  380.],
-                         [985.00009831,  381.],
-                         [985.26987438,  381.],
-                         [985.2156529,  385.]]])
-    test = EDGE(contours)
-    test.quantify()
-    test.connect_edges()
-
-    for edge in test.get_edges():
-        pl.plot(edge['x'], edge['y'])
-
-    pl.show()
